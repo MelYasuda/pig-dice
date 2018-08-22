46ea74d7bf0d151f8fd46f7fe262b8a9703439d9
Mel Yasuda and Victoria Oh <mel@>
2018-08-22 11:55:08 -0700

    Add dice functionality using math.random. Pass spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+  // set player starting score
   var player1 = 0;
   var player2 = 0;
 
+  // disply player score number
   $("#player1-score").append(player1);
   $("#player2-score").append(player2);
 
+  // start button, display player name input
   $("button#start").click(function(event){
     event.preventDefault();
     var player1Name = prompt("Please enter player 1 name.")
@@ -13,8 +16,16 @@ $(document).ready(function(){
     $("#player2").text(player2Name);
   });
 
+  // hide/show the instructions
   $(".well").click(function(event){
     $("p, ul").toggle();
+  });
+
+  // number randomizer
+  $("button#roll").click(function(event){
+    var randomNumber = Math.floor((Math.random() * 6) + 1);
+    console.log(randomNumber);
+    $("#random-number").text(randomNumber);
   });
 
 
