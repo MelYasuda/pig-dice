e3515d2cce6e3a8909e87df6a4ab0fe17ab98e30
Mel Yasuda and Victoria Oh <victoria@>
2018-08-22 11:18:38 -0700

    make the start button to prompt player name and display pass spec 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,26 @@ $(document).ready(function(){
   var player1 = 0;
   var player2 = 0;
 
-  $("#player1").append(player1);
-  $("#player2").append(player2);
+  $("#player1-score").append(player1);
+  $("#player2-score").append(player2);
 
-  
+  $("button#start").click(function(event){
+    event.preventDefault();
+    var player1Name = prompt("Please enter player 1 name.")
+    var player2Name = prompt("Please enter player 2 name.")
+    $("#player1").text(player1Name);
+    $("#player2").text(player2Name);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
